b49af3f5c09ac07c6605c7b279b16673c956ef25
Carlos García Torres <carlos.garcia@kaps.es>
2020-02-08 11:56:51 +0100

    Initial Commit

---
+++ PhotoAlbumExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoAlbumExercise
+//
+//  Created by Carlos García Torres on 08/02/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
